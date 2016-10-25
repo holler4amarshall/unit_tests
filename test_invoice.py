27d5118c6dc624fc9ab6de6d3b792d784d277347
holly <holler4amarshall@hotmail.com>
2016-10-25 06:37:57 +0000

    update function to include value error for 0 hours and a unit test

--- test_invoice.py
+++ test_invoice.py
@@ -9,6 +9,17 @@ class InvoiceCalculatorTests(unittest.TestCase):
         for key in result: 
             if not self.assertEqual((result[key]), (expected_result[key])): 
                 print("pass: {} = {}".format((result[key]), (expected_result[key])))
+                
+                
+    def test_zero_hour_person(self):
+        pay = invoice.divide_pay(360.0, {"Alice": 3.0, "Bob": 6.0, "Carol": 0.0})
+        if not self.assertEqual(pay, {"Alice": 120.0, "Bob": 240.0, "Carol": 0.0}) :
+            print("pass: {} = {}".format(pay, ({"Alice": 120.0, "Bob": 240.0, "Carol": 0.0})))
+            
+    def test_zero_hours_total(self):
+       with self.assertRaises(ValueError):
+           invoice.divide_pay(360.0, {"Alice": 0.0, "Bob": 0.0, "Carol": 0.0})
+        
 
 if __name__ == "__main__":
     unittest.main()
